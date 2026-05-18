133446507377b08e5269634030dc571e010c508f
Fredon99 <fredvss@hotmail.com>
2026-05-17 22:44:41 -0300

    feat: adding features at calculator and starting the refactor in game of stones

--- Course-03/05-genserver/genserver-calculator/calculator.exs
+++ Course-03/05-genserver/genserver-calculator/calculator.exs
@@ -1,57 +1,68 @@
-defmodule Demo do
+defmodule Calculator do
   use GenServer
 
-  # interface function
+  # Starts the GenServer
   def start(initial_state) do
     GenServer.start(__MODULE__, initial_state, name: __MODULE__)
   end
 
-  def sqrt do
-    #GenServer.cast(:calculator, :sqrt)
-    GenServer.cast(__MODULE__, :sqrt)
+  def init(initial_state) when is_number(initial_state) do
+    "I am started with the state #{initial_state}" |> IO.puts
+    {:ok, initial_state}
   end
 
+  def init(_) do
+    {:stop, "The initial state is not a number :("}
+  end
+
+  # Public API
   def add(number) do
     GenServer.cast(__MODULE__, {:add, number})
   end
 
+  def sub(number) do
+    GenServer.cast(__MODULE__, {:sub, number})
+  end
+
+  def sqrt do
+    GenServer.cast(__MODULE__, :sqrt)
+  end
+
   def result do
-    # timeout is 5 seconds
-    GenServer.call __MODULE__, :result
+    GenServer.call(__MODULE__, :result, 5000)
   end
 
   # Synchronous request
   def handle_call(:result, _,  current_state) do
-    { :reply, current_state, current_state }
+    {:reply, current_state, current_state}
   end
 
   def terminate(reason, current_state) do
-    IO.puts "TERMINATED!"
-    reason |> IO.inspect
-    current_state |> IO.inspect
+    IO.puts "Terminating with reason: #{inspect(reason)} and state: #{inspect(current_state)}"
   end
 
   # Asynchronous request
   def handle_cast(:sqrt, current_state) do
-    { :noreply, :math.sqrt(current_state) }
+    {:noreply, :math.sqrt(current_state)}
   end
 
   def handle_cast({:add, number}, current_state) do
-    { :noreply, current_state + number }
+    {:noreply, current_state + number}
   end
 
-  # Callback is run when the server is started
-  def init(initial_state) when is_number(initial_state) do
-    "I am started with the state #{initial_state}" |> IO.puts
-    { :ok, initial_state }
-  end
-
-  def init(_) do
-    { :stop, "The initial state is not a number :(" }
+  def handle_cast({:sub, number}, current_state) do
+    {:noreply, current_state - number}
   end
 end
 
-{:ok, _} = Demo.start(4)
-Demo.sqrt |> IO.inspect
-Demo.add(10) |> IO.inspect
-Demo.result |> IO.inspect
+
+{:ok, _pid} = Calculator.start(4)
+
+Calculator.add(10)
+Calculator.result |> IO.inspect
+
+Calculator.sub(5)
+Calculator.result |> IO.inspect
+
+Calculator.sqrt()
+Calculator.result |> IO.inspect
